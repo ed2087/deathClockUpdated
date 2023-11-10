b0b7e9d6ce00c003c1c53d190c3d5db7ddba3c10
Edgar R <43653240+ed2087@users.noreply.github.com>
2023-11-10 13:17:25 -0500

    Add files via upload

--- public/JS/master.js
+++ public/JS/master.js
@@ -33,3 +33,60 @@ const sendJson = async (url, method, body) => {
     const data = await response.json();
     return data;
 };
+
+
+
+
+
+//////////////////// nav burger function /////////////////////////
+
+//true = open menu / false = close menu
+let menuOpen = false; 
+
+//animation styles
+const slideRight = 'slide-right';
+const slideLeft = 'slide-left';
+
+
+// Toggle menu
+const toggleMenu = () => {
+
+  const nav = document.getElementById('navLargeScreens');
+
+  if (menuOpen) {
+    nav.classList.remove(slideRight); 
+    nav.classList.add(slideLeft);
+    menuOpen = false;
+  } else {
+    nav.classList.remove(slideLeft);
+    nav.classList.add(slideRight);
+    menuOpen = true;
+  }
+
+}
+
+// Close menu if scroll when open
+window.addEventListener('scroll', () => {
+  if (menuOpen) {
+    toggleMenu();
+  }
+});
+
+// Toggle menu on burger click
+document.getElementById('nav_small_burgerButton').addEventListener('click', toggleMenu);
+
+// if screen is bigger than 768px, remove animation classes
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 1300) {
+        nav.classList.remove(slideRight);
+        nav.classList.remove(slideLeft);
+    }
+});
+
+
+const icons = document.querySelectorAll('.icon');
+icons.forEach (icon => {  
+    icon.addEventListener('click', (event) => {
+        icon.classList.toggle("open");        
+    });
+});
---
+++ public/JS/storyPageFun.js
@@ -0,0 +1,145 @@
+console.log('storyPageFun.js loaded');
+
+let page = 1;
+let limit = 8;
+let query = '';
+let language = 'English';
+let totalStories_available = 0;
+let timer;
+
+/////////////////////////////////////////////////
+// Template Functions
+/////////////////////////////////////////////////
+
+const CommonStoryTemplates = (data) => {
+
+    //convert data.createdAt to date display format
+    const date = new Date(data.createdAt);
+    const createdAt = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+
+
+    return `
+        <div class="story">
+            <h1 class="storyTitle">${data.storyTitle}</h1>
+            <h3 class="storySummary">${data.storySummary}</h3>
+            <div class="tags">${data.tags}</div>
+            <div class="extraTags">${data.extraTags}</div>
+            <div class="categories">${data.categories}</div>
+            <div class="legalName">${data.legalName}</div>
+            <div class="upvoteCount">${data.upvoteCount}</div>
+            <div class="createdAt">${createdAt}</div>
+            <a href="#">Not Avalable</a>
+        </div>
+    `;
+};
+
+/////////////////////////////////////////////////
+// Fetch Data from Server
+/////////////////////////////////////////////////
+
+
+let allowOnce = true;
+
+const query_fetch = async () => {
+    const searchInput = id_("search").value;
+    const languageSelect = id_("language").value;
+    const rankingSelect = id_("rankingWrap").value;
+
+    const queryObject = {
+        query: searchInput || '',
+        language: languageSelect || language,
+        ranking: rankingSelect || '',
+        page,
+        limit,
+    };
+
+    const urlParams = new URLSearchParams(queryObject);
+
+    const url = `/terrorTales/query?${urlParams.toString()}`;
+
+    const data = await fetch_(url, 'GET', null);
+    console.log(data);
+
+    if (data.status === 200) {
+
+
+        //allowOnce
+        if (allowOnce) {
+
+            id_('language').innerHTML = '<option value="English">English</option>';
+            //add languagesArray to language select
+            data.languagesArray.forEach((language) => {
+                //if language is english then dont add it to language select
+                if (language === 'English') return;
+                id_('language').innerHTML += `<option value="${language}">${language}</option>`;
+            });
+            
+            allowOnce = false;
+
+        }
+        
+
+        if (page === 1) {
+            id_('storyListWrap').innerHTML = '';           
+
+        }
+
+        const storyListWrap = id_('storyListWrap');
+        if (data.stories.length === 0) {
+            storyListWrap.innerHTML = `
+                <div class="noData">No data found</div>
+            `;
+        } else {
+            
+            data.stories.forEach((data_) => {
+                storyListWrap.innerHTML += CommonStoryTemplates(data_);
+                totalStories_available++;
+            });
+        }
+
+        //if data.totalStories is greater then totalStories_available then dont add load more button
+        if (data.totalStories <= totalStories_available) {
+            //display load more button #loadMore
+            id_('loadMore').style.display = 'none';
+        }else
+        {
+            id_('loadMore').style.display = 'block';
+        }
+
+    } else {
+        console.log('error');
+    }
+};
+
+const debounceQueryFetch = (event) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+        page = 1;
+        totalStories_available = 0;
+        query_fetch(event.target.value);
+    }, 1000);
+};
+
+id_("search").addEventListener('keyup', debounceQueryFetch);
+
+queryAll_('.selectQueryData').forEach((query) => {
+    query.addEventListener('change', (event) => {
+        page = 1;
+        totalStories_available = 0;
+        query_fetch(event.target.value);
+    });
+});
+
+/////////////////////////////////////////////////
+// Fetch Data Pagination
+/////////////////////////////////////////////////
+
+const loadMoreStories = async () => {
+    page++;
+    query_fetch(query, language, ranking, page, limit);
+};
+
+id_('loadMore').addEventListener('click', loadMoreStories);
+
+// Get all the stories initially
+query_fetch();
