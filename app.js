0e7f31e71107f59554c573e021d18f742c663ac4
Edgar R <43653240+ed2087@users.noreply.github.com>
2024-01-22 06:07:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -34,15 +34,16 @@ const app = express();
 
 
 //redirect to https://wwww
-
-
 app.use((req, res, next) => {
-  if (req.headers.host === 'terrorhub.com') {
-    res.redirect(301, 'https://www.terrorhub.com' + req.url);
+  if (req.headers.host !== 'www.terrorhub.com') {
+    res.redirect(301, 'https://www.terrorhub.com' + req.originalUrl);
   } else {
     next();
   }
 });
+
+
+
 
 // Connect to MongoDB
 const DB = process.env.DB_URL;
