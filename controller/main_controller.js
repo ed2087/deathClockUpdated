52faadccfa56f0cf482f222180758592144a6871
Edgar R. <43653240+ed2087@users.noreply.github.com>
2024-08-12 08:01:22 -0400

    Update main_controller.js

--- controller/main_controller.js
+++ controller/main_controller.js
@@ -60,6 +60,7 @@ exports.faq = async function (req, res, next) {
       path: "/faq",
       title: "FAQ",
       headerTitle: "FAQ",
+      description: "TerrorHub - FAQ",
       csrfToken: res.locals.csrfToken,
       userActive,
       userName,
